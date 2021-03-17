e3cf0ea13bfcaf7a0812938e1e0860e58f21e473
Vikas0786 <vthakur412000@gmail.com>
2021-03-17 18:50:49 +0530

    Done with shift hold checkboxes

--- 10-HoldShiftandCheckCheckboxes/index.js
+++ 10-HoldShiftandCheckCheckboxes/index.js
@@ -3,7 +3,22 @@ const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
 let lastChecked;
 
 function handleCheck(e) {
-  console.log(e);
+  let inBetween = false;
+  if (e.shiftKey && this.checked) {
+    checkboxes.forEach((checkbox) => {
+      console.log(checkbox);
+      if (checkbox === this || checkbox === lastChecked) {
+        inBetween = !inBetween;
+        console.log("Starting to check them in between!");
+      }
+
+      if (inBetween) {
+        checkbox.checked = true;
+      }
+    });
+  }
+
+  lastChecked = this;
 }
 
 checkboxes.forEach((checkbox) =>
