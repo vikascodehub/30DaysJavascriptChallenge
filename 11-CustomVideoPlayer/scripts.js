24e076374f2db6e18bcd3f31c063bdf092c3ae00
Vikas0786 <vthakur412000@gmail.com>
2021-03-18 19:23:48 +0530

    updated range buttons

--- 11-CustomVideoPlayer/scripts.js
+++ 11-CustomVideoPlayer/scripts.js
@@ -21,9 +21,17 @@ function skip() {
   video.currentTime += parseFloat(this.dataset.skip);
 }
 
+function handleRangeUpdate() {
+  video[this.name] = this.value;
+}
+
 video.addEventListener("click", togglePlay);
 video.addEventListener("play", updateButton);
 video.addEventListener("pause", updateButton);
-//video.addEventListener("timeupdate", handleProgress);
+
 toggle.addEventListener("click", togglePlay);
 skipButtons.forEach((button) => button.addEventListener("click", skip));
+ranges.forEach((range) => range.addEventListener("change", handleRangeUpdate));
+ranges.forEach((range) =>
+  range.addEventListener("mousemove", handleRangeUpdate)
+);
